597fe3ad394d05133c5a2a665afdb0126e4b2e85
diandra6695 <andracom108@gmail.com>
2024-02-19 16:38:34 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1F1F1F",
+        secondary: "#F3F3F3",
+      },
+    },
+  },
+  plugins: [],
+};
